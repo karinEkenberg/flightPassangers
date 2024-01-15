d412d81d81c5263d19551e8318012a93e2fa27ff
karinEkenberg <zirkon6@hotmail.com>
2024-01-15 11:00:28 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static flightPassangers.FlightPatterns;
+namespace flightPassangers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Passanger[] passangers =
+            {
+                new FirstClass {AirMiles =1_49, Name = "Karen"},
+                new FirstClass {AirMiles = 16_562, Name = "Jan"},
+                new BusinessClass {Name = "Molly"},
+                new CoachClass {CarryOnKG = 22.2, Name = "Anna"},
+                new CoachClass {CarryOnKG = 0, Name = "Ubbe"}
+            };
+
+            foreach (Passanger passanger in passangers)
+            {
+                decimal flightCost = passanger switch
+                {
+                    FirstClass p when p.AirMiles > 35000 => 1500M,
+                    FirstClass p when p.AirMiles > 15000 => 1750M,
+                    FirstClass _                         => 2000M,
+                    BusinessClass _                      => 1000M,
+                    CoachClass p when p.CarryOnKG < 10.0 => 500M,
+                    CoachClass _                         => 650M,
+                    _                                    => 800M,
+                };
+                Console.WriteLine($"Flight cost: {flightCost:C} for {passanger}");
+            }
+        }
+    }
+}
---
+++ flightPatterns.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static flightPassangers.FlightPatterns;
+
+namespace flightPassangers
+{
+    internal class FlightPatterns
+    {
+        public class Passanger
+        {
+            public string? Name { get; set; }
+        }
+
+        public class BusinessClass : Passanger
+        {
+            public override string ToString()
+            {
+                return $"Business Class: {Name}";
+            }
+        }
+
+        public class FirstClass : Passanger
+        {
+            public int AirMiles { get; set; }
+            public override string ToString()
+            {
+                return $"First Class with {AirMiles:N0} air miles: {Name}";
+            }
+        }
+
+        public class  CoachClass : Passanger
+        {
+            public double CarryOnKG { get; set; }
+            public override string ToString()
+            {
+                return $"Coach Class with {CarryOnKG} KG carry on: {Name}";
+            }
+        }
+
+    }
+
+
+}
